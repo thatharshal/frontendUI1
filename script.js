f62669246641688e52bb385c2617265a8ff3a4a7
Harshal Chaudhari <122505749+thatharshal@users.noreply.github.com>
2024-09-10 22:57:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// gsap.from("#rtext",{
+//     y: 900,
+//     duration: 6,
+//     delay: 0.1,
+// })
+// gsap.to("#rtext",{
+//     y: -900,
+//     duration: 5,
+//     delay: 0.5,
+//     repeat:-1,
+// })
